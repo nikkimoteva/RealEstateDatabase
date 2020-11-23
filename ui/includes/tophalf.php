3223306c753ac4c1b7fe7825a5e25d3ebcbc875c
phillipsngs <49970286+phillipsngs@users.noreply.github.com>
2020-11-23 02:26:34 -0800

    Fixed typo on anchor

--- ui/includes/tophalf.php
+++ ui/includes/tophalf.php
@@ -124,7 +124,7 @@
                          </select>
                         <li class = "nav-item" id = "agentT"> <a class = "nav-link mb-2 text-muted" id = "agentLink" >Agent Represents</a> </li>
                             <li class = "nav-item" id = "amenityT"> <a class = "nav-link mb-2 text-muted" href = "amenity/amenityView.php">Amenity</a> </li>
-                            <li class = "nav-item" id = "apartmentsT"> <a class = "nav-link mb-2 text-muted" href = "apartments/apartmentView.php">Appartments</a> </li>
+                            <li class = "nav-item" id = "apartmentsT"> <a class = "nav-link mb-2 text-muted" href = "apartments/apartmentView.php">Apartments</a> </li>
                             <li class = "nav-item" id = "appointsT" > <a class = "nav-link mb-2 text-muted" href = "appoints/appointsView.php">Appoints</a> </li>
                             <li class = "nav-item" id = "branchT" > <a class = "nav-link mb-2 text-muted" href = "branch/branchView.php">Branch</a> </li>
                             <li class = "nav-item" id = "branchLocationT"> <a class = "nav-link mb-2 text-muted" href = "branchLocation/branchLocationView.php">Branch Location</a> </li>
